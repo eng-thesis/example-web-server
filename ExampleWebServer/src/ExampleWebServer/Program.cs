54dcc832d95095512aee621e630741b01904d878
Jakub Frączek <jakub.f102@gmail.com>
2026-04-06 20:10:33 +0200

    Feat: Added db connection in Program.cs

--- ExampleWebServer/src/ExampleWebServer/Program.cs
+++ ExampleWebServer/src/ExampleWebServer/Program.cs
@@ -1,3 +1,9 @@
+using ExampleWebServer.Data;
+using ExampleWebServer.Domain.Repositories;
+using ExampleWebServer.Repositories;
+using ExampleWebServer.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,5 +18,19 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<DataContext>();
+    db.Database.EnsureCreated();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<DataContext>();
+    await Seed.SeedData(db);
+}
+
+app.MapControllers();
 app.UseHttpsRedirection();
 app.Run();
